075ad09ef0af80fc74118a5bff443e01662496d2
Michael P. Jung <michael.jung@terreon.de>
2025-02-12 08:26:29 +0000

    Revert "Allow configuring Redis connections via AsyncConnectionConfig"
    This reverts commit ac5d03dd295f99f1895d13ae798320dfa1d4667f.

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -36,7 +36,7 @@ use std::{
 use deadpool::managed;
 use redis::{
     aio::{ConnectionLike, MultiplexedConnection},
-    AsyncConnectionConfig, Client, IntoConnectionInfo, RedisError, RedisResult,
+    Client, IntoConnectionInfo, RedisError, RedisResult,
 };
 
 pub use redis;
@@ -127,20 +127,10 @@ impl ConnectionLike for Connection {
 /// [`Manager`] for creating and recycling [`redis`] connections.
 ///
 /// [`Manager`]: managed::Manager
+#[derive(Debug)]
 pub struct Manager {
     client: Client,
     ping_number: AtomicUsize,
-    connection_config: AsyncConnectionConfig,
-}
-
-// `redis::AsyncConnectionConfig: !Debug`
-impl std::fmt::Debug for Manager {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Manager")
-            .field("client", &self.client)
-            .field("ping_number", &self.ping_number)
-            .finish()
-    }
 }
 
 impl Manager {
@@ -150,22 +140,9 @@ impl Manager {
     ///
     /// If establishing a new [`Client`] fails.
     pub fn new<T: IntoConnectionInfo>(params: T) -> RedisResult<Self> {
-        Self::from_config(params, AsyncConnectionConfig::default())
-    }
-
-    /// Creates a new [`Manager`] from the given `params` and [`AsyncConnectionConfig`].
-    ///
-    /// # Errors
-    ///
-    /// If establishing a new [`Client`] fails.
-    pub fn from_config<T: IntoConnectionInfo>(
-        params: T,
-        connection_config: AsyncConnectionConfig,
-    ) -> RedisResult<Self> {
         Ok(Self {
             client: Client::open(params)?,
             ping_number: AtomicUsize::new(0),
-            connection_config,
         })
     }
 }
@@ -175,10 +152,7 @@ impl managed::Manager for Manager {
     type Error = RedisError;
 
     async fn create(&self) -> Result<MultiplexedConnection, RedisError> {
-        let conn = self
-            .client
-            .get_multiplexed_async_connection_with_config(&self.connection_config)
-            .await?;
+        let conn = self.client.get_multiplexed_async_connection().await?;
         Ok(conn)
     }
 
